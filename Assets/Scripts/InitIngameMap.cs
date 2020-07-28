6ca012ff683f9b21444b2b143d367dd21185067c
1cekrim <1cekrim.dev@gmail.com>
2020-07-28 16:45:36 +0900

    CreateTile 메소드 추가

--- Assets/Scripts/InitIngameMap.cs
+++ Assets/Scripts/InitIngameMap.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class InitIngameMap : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject[] tileObjects = new GameObject[2];
+    public int gridRows;
+    public int gridCols;
+    public int cellSize;
+    private GameObject[,] tiles;
 
-    // Update is called once per frame
-    void Update()
+    private void CreateTile()
     {
-        
+        // TODO: 맵을 처음 생성할 때는 특수효과 넣기
+        Debug.Log("InitIngameMap::CreateTile");
+        tiles = new GameObject[gridRows, gridCols];
+        for (int i = 0; i < gridRows; ++i)
+        {
+            for (int j = 0; j < gridCols; ++j)
+            {
+                tiles[i, j] = Instantiate(tileObjects[(i + j) % 2], new Vector3(j, 0, i) * cellSize, Quaternion.identity) as GameObject;
+            }
+        }
     }
 }
