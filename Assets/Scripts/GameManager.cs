0ff1f9d2864fb106d9a5876c7770dd8205062df2
1cekrim <1cekrim.dev@gmail.com>
2020-07-29 11:34:52 +0900

    GameManager 구현

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameManager : MonoBehaviour
+{
+    public UnityEvent awakeEvents;
+    public UnityEvent startEvents;
+    public UnityEvent tickEvents;
+    private static GameManager _instance;
+    private int fixedUpdateCounter = 0;
+    public static GameManager Instance
+    {
+        get {
+            if (!_instance)
+            {
+                _instance = FindObjectOfType(typeof(GameManager)) as GameManager;
+            }
+            return _instance;
+        }
+    }
+    private void Awake()
+    {
+        Debug.Log(Time.fixedDeltaTime);
+        if (!_instance)
+        {
+            _instance = this;
+        }
+        else if (_instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+
+        awakeEvents.Invoke();
+    }
+    private void Start()
+    {
+        startEvents.Invoke();
+    }
+
+    private void FixedUpdate()
+    {
+        if (++fixedUpdateCounter % 10 == 0)
+        {
+            tickEvents.Invoke();
+        }
+    }
+}
