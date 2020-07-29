042ee99dc19d8a587d491239d3d99fdd438c7d38
1cekrim <1cekrim.dev@gmail.com>
2020-07-29 12:25:25 +0900

    맵 주변에 바다 생기게 변경

--- Assets/Scripts/InitIngameMap.cs
+++ Assets/Scripts/InitIngameMap.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class InitIngameMap : MonoBehaviour
 {
     public GameObject[] tileObjects = new GameObject[2];
+    public GameObject seaObject;
+    public GameObject mapParent;
     public int gridRows;
     public int gridCols;
     public int cellSize;
+    public int waterPadding;
     private GameObject[,] tiles;
 
     public void CreateTile()
@@ -15,11 +18,22 @@ public class InitIngameMap : MonoBehaviour
         // TODO: 맵을 처음 생성할 때는 특수효과 넣기
         Debug.Log("InitIngameMap::CreateTile");
         tiles = new GameObject[gridRows, gridCols];
-        for (int i = 0; i < gridRows; ++i)
+        Quaternion seaRotation = Quaternion.identity;
+        seaRotation.eulerAngles = new Vector3(90, 0, 0);
+        for (int i = -waterPadding; i < gridRows + waterPadding; ++i)
         {
-            for (int j = 0; j < gridCols; ++j)
+            for (int j = -waterPadding; j < gridCols + waterPadding; ++j)
             {
-                tiles[i, j] = Instantiate(tileObjects[(i + j) % 2], new Vector3(j, 0, i) * cellSize, Quaternion.identity) as GameObject;
+                if (i >= 0 && i < gridRows && j >= 0 && j < gridCols)
+                {
+                    tiles[i, j] = Instantiate(tileObjects[(i + j) % 2], new Vector3(j, 0, i) * cellSize, Quaternion.identity) as GameObject;
+                    tiles[i, j].transform.parent = mapParent.transform;
+                }
+                else
+                {
+                    var obj = Instantiate(seaObject, new Vector3(j, 0, i) * cellSize, seaRotation) as GameObject;
+                    obj.transform.parent = mapParent.transform;
+                }
             }
         }
     }
