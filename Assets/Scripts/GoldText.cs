0caeb6779a4b5a54b0c8c7afa48c7c05c3aea691
1cekrim <1cekrim.dev@gmail.com>
2020-07-31 01:07:32 +0900

    GoldText를 수정하는 스크립트 작성 후, ChangeGold 이벤트 핸들러로 등록

---
+++ Assets/Scripts/GoldText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoldText : MonoBehaviour
+{
+    private Text text;
+    private void Awake()
+    {
+        text = GetComponent<Text>();
+    }
+    private void Start()
+    {
+        UpdateText();
+    }
+    public void UpdateText()
+    {
+        text.text = ResourceManager.Instance.Gold.ToString();
+    }
+}
