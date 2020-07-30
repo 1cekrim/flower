46db21bf37da01295d296ee957490a811da5ad16
1cekrim <1cekrim.dev@gmail.com>
2020-07-30 22:55:43 +0900

    ResourceManager 클래스 추가

---
+++ Assets/Scripts/ResourceManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour
+{
+    private static ResourceManager _instance;
+    public static ResourceManager Instance
+    {
+        get {
+            if (!_instance)
+            {
+                _instance = FindObjectOfType(typeof(ResourceManager)) as ResourceManager;
+            }
+            return _instance;
+        }
+    }
+    private void Awake()
+    {
+        if (!_instance)
+        {
+            _instance = this;
+        }
+        else if (_instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+}
