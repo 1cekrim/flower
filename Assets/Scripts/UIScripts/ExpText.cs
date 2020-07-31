eb0a20b91b79221725527ccfedd60915da1c982a
1cekrim <1cekrim.dev@gmail.com>
2020-07-31 14:16:59 +0900

    ExpText를 수정하는 스크립트 작성 후, ChangeExp와 LevelUp 이벤트 핸들러로 등록

---
+++ Assets/Scripts/UIScripts/ExpText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExpText : MonoBehaviour
+{
+    private Text text;
+    private void Awake()
+    {
+        text = GetComponent<Text>();
+    }
+    private void Start()
+    {
+        UpdateText();
+    }
+    public void UpdateText()
+    {
+        text.text = ResourceManager.Instance.Exp.ToString() + "/" + ResourceManager.Instance.MaxExp.ToString();
+    }
+}
