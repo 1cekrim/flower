2fcd1cbaa08301b8ea851ce1c9e95d4a9834af0c
1cekrim <1cekrim.dev@gmail.com>
2020-07-31 16:25:34 +0900

    Item 클래스 추가

---
+++ Assets/Scripts/ItemScripts/Item.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField]
+    private RawImage itemImage;
+    [SerializeField]
+    private Text itemText;
+    [SerializeField]
+    private Text itemCount;
+    private int  itemCountInt;
+    [SerializeField]
+    private GameObject itemCountBox;
+    [SerializeField]
+    
+    public Texture ItemTexture
+    {
+        get
+        {
+            return itemImage.texture;
+        }
+        set
+        {
+            itemImage.texture = value;
+        }
+    }
+
+    public string ItemName
+    {
+        get
+        {
+            return itemText.text;
+        }
+        set
+        {
+            itemText.text = value;
+        }
+    }
+
+    public int ItemCount
+    {
+        get
+        {
+            return itemCountInt;
+        }
+        set
+        {
+            itemCountInt = value;
+            itemCount.text = value.ToString();
+            itemCountBox.SetActive(value > 1);
+        }
+    }
+}
