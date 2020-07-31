718dba7fd47b443f2434843211e97cede9656f3d
1cekrim <1cekrim.dev@gmail.com>
2020-07-31 15:12:39 +0900

    ItradeAble 인터페이스 추가

---
+++ Assets/Scripts/ItemInterfaces.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public interface ITradeAble
+{
+    int PurchasePrice { get; }
+    int SalePrice { get; }
+    bool Purchase(int amount = 1);
+    bool Sale(int amount = 1);
+}
