f7874050d80ae5e44ce5e81c77205d089ca83fd6
1cekrim <1cekrim.dev@gmail.com>
2020-07-31 14:26:23 +0900

    SprinkerText를 수정하는 스크립트 작성 후, ChangeSprinkler와 ChangeMaxSprinkler 이벤트 핸들러로 등록

---
+++ Assets/Scripts/UIScripts/SprinklerText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SprinklerText : MonoBehaviour
+{
+    private Text text;
+    private void Awake()
+    {
+        text = GetComponent<Text>();
+    }
+    private void Start()
+    {
+        UpdateText();
+    }
+    public void UpdateText()
+    {
+        text.text = ResourceManager.Instance.Sprinkler.ToString() + "/" + ResourceManager.Instance.MaxSprinkler.ToString();
+    }
+}
