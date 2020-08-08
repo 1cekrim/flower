3267fcfbc1dbfb04f6856e4b8637ce3c5f53b918
1cekrim <1cekrim.dev@gmail.com>
2020-08-08 15:06:35 +0900

    DOTween으로 다이얼로그 구현

--- Assets/Scripts/UIScripts/InteractDialog.cs
+++ Assets/Scripts/UIScripts/InteractDialog.cs
@@ -6,8 +6,45 @@ using DG.Tweening;
 
 public class InteractDialog : MonoBehaviour
 {
-    public void Up()
+    [SerializeField]
+    private float hidePosY;
+    [SerializeField]
+    private float moveDialogTime;
+    [SerializeField]
+    private GameObject dialogPanel;
+    private Image dialogPanelImage;
+    private Button dialogPanelButton;
+    private Sequence lastSequence;
+    private void Awake()
     {
-
+        hidePosY = -gameObject.GetComponent<RectTransform>().rect.height;
+        dialogPanelImage = dialogPanel.GetComponent<Image>();
+        dialogPanelButton = dialogPanel.GetComponent<Button>();
+    }
+    public void MoveDialog(bool isUp)
+    {
+        lastSequence?.Kill();
+        if (isUp)
+        {
+            lastSequence = DOTween.Sequence()
+                                  .OnStart(() =>
+                                  {
+                                      dialogPanelImage.canvasRenderer.SetAlpha(0);
+                                      dialogPanel.SetActive(true);
+                                      dialogPanelButton.interactable = true;
+                                      dialogPanelImage.CrossFadeAlpha(0.5f, moveDialogTime, false);
+                                  })
+                                  .Join(transform.DOMoveY(0, moveDialogTime))
+                                  .Play();
+        }
+        else
+        {
+            dialogPanelButton.interactable = false;
+            lastSequence = DOTween.Sequence()
+                                  .OnStart(() => dialogPanelImage.CrossFadeAlpha(0, moveDialogTime, false))
+                                  .Join(transform.DOMoveY(hidePosY, moveDialogTime))
+                                  .OnComplete(() => dialogPanel.SetActive(false))
+                                  .Play();
+        }
     }
 }
